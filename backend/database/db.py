e010bdfbe7be674e2afa8f17d5e5248350e2cdf7
mohanShyam-developer <dammumohan246@gmail.com>
2026-06-17 06:46:04 +0530

    Update TiDB connection

--- backend/database/db.py
+++ backend/database/db.py
@@ -3,6 +3,7 @@ import os
 
 db = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT", 4000)),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME")
